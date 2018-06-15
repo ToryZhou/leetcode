a8f0610a70925ef64aa8b25e1575591e2f87838f
toryzhou <522617505@qq.com>
2018-06-15 20:21:06 +0800

    Add palindrome number

---
+++ src/main/java/com/job/leetcode/palindromenumber/PalindromeNumber.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.palindromenumber;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        int last = x%10;
+        return true;
+    }
+
+
+}
