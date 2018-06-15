6745846b7f24e5c676f02da4c9a23399377b4220
toryzhou <522617505@qq.com>
2018-06-15 19:37:08 +0800

    Add best time to by and sell stock

---
+++ src/main/java/com/job/leetcode/besttimetobuyandsellstock/Stock.java
@@ -0,0 +1,29 @@
+package com.job.leetcode.besttimetobuyandsellstock;
+
+
+public class Stock {
+
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            int max = rightBiggest(prices, i);
+            if (max > prices[i]) {
+                if (max - prices[i] > result) {
+                    result = max - prices[i];
+                }
+            }
+        }
+        return result;
+    }
+
+    private int rightBiggest(int[] prices, int index) {
+        int max = 0;
+        for (; index < prices.length; index++) {
+            if (prices[index] > max) {
+                max = prices[index];
+            }
+        }
+        return max;
+    }
+
+}
---
+++ src/test/java/com/job/leetcode/besttimetobuyandsellstock/StockTest.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.besttimetobuyandsellstock;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StockTest {
+
+    @Test
+    public void test(){
+        Stock stock = new Stock();
+        Assert.assertEquals(5,stock.maxProfit(new int[]{7,1,5,3,6,4}));
+        Assert.assertEquals(0,stock.maxProfit(new int[]{7,6,4,3,1}));
+    }
+}
