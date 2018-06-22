2cedba0daa0d36f64ebe921d74a7404b7da358a5
toryzhou <522617505@qq.com>
2018-06-22 22:34:39 +0800

    Edit count and say

--- src/main/java/com/job/leetcode/countandsay/CountAndSay.java
+++ src/main/java/com/job/leetcode/countandsay/CountAndSay.java
@@ -7,23 +7,27 @@ public class CountAndSay {
     // todo
     public String countAndSay(int n) {
         String s = "1";
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             char[] chars = s.toCharArray();
             char temp = chars[0];
+            char nexTemp = chars[0];
             int size = 0;
+            int nextSize = 0;
             boolean end = false;
             for (int j = 0; j < chars.length; j++) {
                 if (temp == chars[j]) {
                     size++;
                 } else {
                     end = true;
-                    temp = chars[j];
+                    nexTemp = chars[j];
+                    nextSize++;
                 }
                 if (j == chars.length - 1) {
                     end = true;
                 }
                 if (end) {
                     s = size + "" + temp;
+                    temp = chars[j];
                     size = 0;
                 }
             }
--- src/test/java/com/job/leetcode/countandsay/CountAndSayTest.java
+++ src/test/java/com/job/leetcode/countandsay/CountAndSayTest.java
@@ -8,7 +8,7 @@ public class CountAndSayTest {
     @Test
     public void test(){
         CountAndSay countAndSay = new CountAndSay();
-        String s = countAndSay.countAndSay(3);
+        String s = countAndSay.countAndSay(4);
         System.out.println(s);
     }
 
