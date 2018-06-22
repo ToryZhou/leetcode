6522df39fe7408b585bf6ed8adde685494be7729
torychow <522617505@qq.com>
2018-06-23 01:47:28 +0800

    Finish count and say

--- src/main/java/com/job/leetcode/countandsay/CountAndSay.java
+++ src/main/java/com/job/leetcode/countandsay/CountAndSay.java
@@ -4,34 +4,34 @@ package com.job.leetcode.countandsay;
  * https://leetcode-cn.com/problems/count-and-say/description/
  */
 public class CountAndSay {
-    // todo
     public String countAndSay(int n) {
         String s = "1";
         for (int i = 0; i < n - 1; i++) {
-            char[] chars = s.toCharArray();
-            char temp = chars[0];
-            char nexTemp = chars[0];
-            int size = 0;
-            int nextSize = 0;
-            boolean end = false;
-            for (int j = 0; j < chars.length; j++) {
-                if (temp == chars[j]) {
-                    size++;
-                } else {
-                    end = true;
-                    nexTemp = chars[j];
-                    nextSize++;
-                }
-                if (j == chars.length - 1) {
-                    end = true;
-                }
-                if (end) {
-                    s = size + "" + temp;
-                    temp = chars[j];
-                    size = 0;
-                }
-            }
+            s = calc(s);
         }
         return s;
+    }
+
+    private String calc(String s) {
+        StringBuilder result = new StringBuilder();
+        char[] chars = s.toCharArray();
+        int size = 1;
+        boolean end = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (i < chars.length - 1 && chars[i + 1] == chars[i]) {
+                size++;
+            } else {
+                end = true;
+            }
+            if (i == chars.length - 1) {
+                end = true;
+            }
+            if (end) {
+                result.append(size).append(chars[i]);
+                size = 1;
+                end = false;
+            }
+        }
+        return result.toString();
     }
 }
--- src/test/java/com/job/leetcode/countandsay/CountAndSayTest.java
+++ src/test/java/com/job/leetcode/countandsay/CountAndSayTest.java
@@ -1,15 +1,19 @@
 package com.job.leetcode.countandsay;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CountAndSayTest {
 
 
     @Test
-    public void test(){
+    public void test() {
         CountAndSay countAndSay = new CountAndSay();
-        String s = countAndSay.countAndSay(4);
-        System.out.println(s);
+        Assert.assertEquals("1", countAndSay.countAndSay(1));
+        Assert.assertEquals("11", countAndSay.countAndSay(2));
+        Assert.assertEquals("21", countAndSay.countAndSay(3));
+        Assert.assertEquals("1211", countAndSay.countAndSay(4));
+        Assert.assertEquals("111221", countAndSay.countAndSay(5));
     }
 
 }
