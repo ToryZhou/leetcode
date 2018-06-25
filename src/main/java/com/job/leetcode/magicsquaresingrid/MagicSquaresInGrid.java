d5e61832e2eae88e8a5358a646ce398ede7e5287
toryzhou <522617505@qq.com>
2018-06-25 12:36:43 +0800

    Add magic squares in grid

---
+++ src/main/java/com/job/leetcode/magicsquaresingrid/MagicSquaresInGrid.java
@@ -0,0 +1,22 @@
+package com.job.leetcode.magicsquaresingrid;
+
+/**
+ * https://leetcode-cn.com/problems/magic-squares-in-grid/description/
+ */
+public class MagicSquaresInGrid {
+    public int numMagicSquaresInside(int[][] grid) {
+        int xMaxIndex = grid[0].length - 3;
+        int yMaxIndex = grid.length - 3;
+        for (int xIndex = 0; xIndex < xMaxIndex; xIndex++) {
+            for (int yIndex = 0; yIndex < yMaxIndex; yIndex++) {
+
+
+            }
+
+        }
+    }
+
+    public boolean isMagicSquare(int[][] square){
+
+    }
+}
