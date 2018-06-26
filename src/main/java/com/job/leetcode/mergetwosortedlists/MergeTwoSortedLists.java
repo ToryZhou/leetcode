42c0107ae716e6008830c57062537bddfa734f2a
torychow <522617505@qq.com>
2018-06-27 01:47:14 +0800

    Add the link for the merge two sorted lists

--- src/main/java/com/job/leetcode/mergetwosortedlists/MergeTwoSortedLists.java
+++ src/main/java/com/job/leetcode/mergetwosortedlists/MergeTwoSortedLists.java
@@ -1,5 +1,8 @@
 package com.job.leetcode.mergetwosortedlists;
 
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ */
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
