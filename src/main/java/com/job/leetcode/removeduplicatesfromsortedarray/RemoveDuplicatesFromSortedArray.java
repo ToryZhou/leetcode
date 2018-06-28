7620f4b710a887e9eae113c5b255202c62ece005
torychow <522617505@qq.com>
2018-06-29 00:24:14 +0800

    Add remove duplicates from sorted array

---
+++ src/main/java/com/job/leetcode/removeduplicatesfromsortedarray/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,21 @@
+package com.job.leetcode.removeduplicatesfromsortedarray;
+
+/**
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/description/
+ */
+public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length - 1; ) {
+            if (nums[i] == nums[i + 1]) {
+                System.arraycopy(nums, i + 1, nums, i, length - 1 - i);
+                length--;
+            } else {
+                i++;
+            }
+        }
+        return length;
+    }
+
+}
---
+++ src/test/java/com/job/leetcode/removeduplicatesfromsortedarray/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.removeduplicatesfromsortedarray;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveDuplicatesFromSortedArrayTest {
+
+    @Test
+    public void test() {
+        RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+        Assert.assertEquals(2, removeDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 1, 2}));
+        Assert.assertEquals(5, removeDuplicatesFromSortedArray.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
+    }
+}
