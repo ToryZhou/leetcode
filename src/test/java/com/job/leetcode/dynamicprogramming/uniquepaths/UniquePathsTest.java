b645cab56fae514b285f8e72b69bdea6394c939a
torychow <522617505@qq.com>
2018-07-02 01:29:44 +0800

    Add unique paths

--- src/test/java/com/job/leetcode/dynamicprogramming/uniquepaths/UniquePathsTest.java
+++ src/test/java/com/job/leetcode/dynamicprogramming/uniquepaths/UniquePathsTest.java
@@ -3,9 +3,5 @@ package com.job.leetcode.dynamicprogramming.uniquepaths;
 /**
  * https://leetcode-cn.com/problems/unique-paths/description/
  */
-public class UniquePaths {
-
-    public int uniquePaths(int m, int n) {
-        return 0;
-    }
+public class UniquePathsTest {
 }
