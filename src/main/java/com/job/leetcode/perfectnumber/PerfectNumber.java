7d6bf09a9f17a87da40144d3b1db50c6413d2c22
torychow <522617505@qq.com>
2018-07-01 15:27:34 +0800

    Edit perfect number

--- src/main/java/com/job/leetcode/perfectnumber/PerfectNumber.java
+++ src/main/java/com/job/leetcode/perfectnumber/PerfectNumber.java
@@ -1,32 +1,43 @@
 package com.job.leetcode.perfectnumber;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * https://leetcode-cn.com/problems/perfect-number/description/
  */
 public class PerfectNumber {
 
-    // todo
     public boolean checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        int i = 1;
+        for (; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                sum += num / i;
+            }
+        }
+        if ((i - 1) * (i - 1) == num) {
+            sum = sum - (i - 1);
+        }
+        return sum - num == num;
+    }
+
+
+    public boolean checkPerfectNumberLower(int num) {
         if (num == 0) {
             return false;
         }
         int sum = 0;
-        List<Integer> integers = new ArrayList<>();
         int temp = num / 2;
         for (int i = 1; i < temp; i++) {
             if (num % i == 0) {
-                integers.add(i);
-                integers.add(num / i);
+                sum += i;
+                sum += num / i;
                 if (num / i <= temp) {
                     temp = num / i;
                 }
             }
-        }
-        for (int i : integers) {
-            sum += i;
         }
         return sum == num * 2;
     }
