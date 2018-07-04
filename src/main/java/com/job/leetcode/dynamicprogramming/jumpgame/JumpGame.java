5767701dd7821e38f4dfce61f1e7ca7ac2cdc346
toryzhou <522617505@qq.com>
2018-07-04 19:37:11 +0800

    Add jump game

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/jumpgame/JumpGame.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.dynamicprogramming.jumpgame;
+
+/**
+ * Auther: toryzhou
+ * Date: 7/4/18 19:36
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+}
