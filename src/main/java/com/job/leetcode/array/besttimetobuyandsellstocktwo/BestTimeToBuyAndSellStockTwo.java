4ef37cf3175b92291a98700c0d2091c951302433
torychow <522617505@qq.com>
2018-07-07 21:07:40 +0800

    Add best time to buy and sell stock two

--- src/main/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwo.java
+++ src/main/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwo.java
@@ -6,4 +6,22 @@ package com.job.leetcode.array.besttimetobuyandsellstocktwo;
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/description/
  */
 public class BestTimeToBuyAndSellStockTwo {
+
+    public int maxProfit(int[] prices) {
+        int buyPrice = 0;
+        int earn = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i] && buyPrice == 0) {
+                buyPrice = prices[i];
+            }
+            if (prices[i + 1] < prices[i] && buyPrice != 0) {
+                earn += prices[i] - buyPrice;
+                buyPrice = 0;
+            }
+        }
+        if (buyPrice != 0) {
+            earn += prices[prices.length - 1] - buyPrice;
+        }
+        return earn;
+    }
 }
--- src/test/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwoTest.java
+++ src/test/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwoTest.java
@@ -1,4 +1,15 @@
 package com.job.leetcode.array.besttimetobuyandsellstocktwo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BestTimeToBuyAndSellStockTwoTest {
+
+    @Test
+    public void test() {
+        BestTimeToBuyAndSellStockTwo bestTimeToBuyAndSellStockTwo = new BestTimeToBuyAndSellStockTwo();
+        Assert.assertEquals(7, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        Assert.assertEquals(4, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{1, 2, 3, 4, 5}));
+        Assert.assertEquals(0, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
 }
