40dde57d86c1132c0b5586780de1f3be14120501
torychow <522617505@qq.com>
2018-07-07 21:14:28 +0800

    Edit best time to buy and sell stock two

--- src/main/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwo.java
+++ src/main/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwo.java
@@ -8,18 +8,28 @@ package com.job.leetcode.array.besttimetobuyandsellstocktwo;
 public class BestTimeToBuyAndSellStockTwo {
 
     public int maxProfit(int[] prices) {
-        int buyPrice = 0;
-        int earn = 0;
-        for (int i = 0; i < prices.length - 1; i++) {
-            if (prices[i + 1] > prices[i] && buyPrice == 0) {
-                buyPrice = prices[i];
-            }
-            if (prices[i + 1] < prices[i] && buyPrice != 0) {
-                earn += prices[i] - buyPrice;
-                buyPrice = 0;
+        int res = 0;
+        for (int i = 0; i < prices.length - 1; ++i) {
+            if (prices[i] < prices[i + 1]) {
+                res += prices[i + 1] - prices[i];
             }
         }
-        if (buyPrice != 0) {
+        return res;
+    }
+
+    public int maxProfitLower(int[] prices) {
+        int buyPrice = -1;
+        int earn = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i] && buyPrice == -1) {
+                buyPrice = prices[i];
+            }
+            if (prices[i + 1] < prices[i] && buyPrice != -1) {
+                earn += prices[i] - buyPrice;
+                buyPrice = -1;
+            }
+        }
+        if (buyPrice != -1) {
             earn += prices[prices.length - 1] - buyPrice;
         }
         return earn;
--- src/test/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwoTest.java
+++ src/test/java/com/job/leetcode/array/besttimetobuyandsellstocktwo/BestTimeToBuyAndSellStockTwoTest.java
@@ -11,5 +11,6 @@ public class BestTimeToBuyAndSellStockTwoTest {
         Assert.assertEquals(7, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
         Assert.assertEquals(4, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{1, 2, 3, 4, 5}));
         Assert.assertEquals(0, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{7, 6, 4, 3, 1}));
+        Assert.assertEquals(2, bestTimeToBuyAndSellStockTwo.maxProfit(new int[]{2, 1, 2, 0, 1}));
     }
 }
