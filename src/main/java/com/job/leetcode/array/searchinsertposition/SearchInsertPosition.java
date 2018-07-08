3f55acdbc4e8179568caf7236e4226ab397baa33
toryzhou <522617505@qq.com>
2018-07-09 01:48:41 +0800

    Add search insert position

---
+++ src/main/java/com/job/leetcode/array/searchinsertposition/SearchInsertPosition.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.searchinsertposition;
+
+/**
+ * Auther: toryzhou
+ * Date: 7/9/18 01:47
+ */
+public class SearchInsertPosition {
+    //todo
+    public int searchInsert(int[] nums, int target) {
+        return 0;
+    }
+}
