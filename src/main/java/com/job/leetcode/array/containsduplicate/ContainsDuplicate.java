0f5f5b746117fea23a7816db85ba166d0919ceae
torychow <522617505@qq.com>
2018-07-10 02:26:19 +0800

    Add contains duplicate

---
+++ src/main/java/com/job/leetcode/array/containsduplicate/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.containsduplicate;
+
+/**
+ * https://leetcode-cn.com/problems/contains-duplicate/description/
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+        return false;
+    }
+}
