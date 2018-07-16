1ee09d2a2252d6a0c1cd1d23ac1fd18978c690d2
torychow <522617505@qq.com>
2018-07-17 00:44:15 +0800

    Add intersection of two arrays ii

---
+++ src/main/java/com/job/leetcode/array/intersectionoftwoarraysii/IntersectionOfTwoArraysII.java
@@ -0,0 +1,10 @@
+package com.job.leetcode.array.intersectionoftwoarraysii;
+
+/**
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/description/
+ */
+public class IntersectionOfTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        return null;
+    }
+}
