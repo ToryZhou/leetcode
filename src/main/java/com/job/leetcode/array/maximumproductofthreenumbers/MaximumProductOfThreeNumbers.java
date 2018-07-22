c4c0718a044678a5b9e6b294c910387caa0f0567
torychow <522617505@qq.com>
2018-07-23 00:22:48 +0800

    Add maximum product of three numbers

---
+++ src/main/java/com/job/leetcode/array/maximumproductofthreenumbers/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.maximumproductofthreenumbers;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-product-of-three-numbers/description/
+ */
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+
+        return 0;
+    }
+}
