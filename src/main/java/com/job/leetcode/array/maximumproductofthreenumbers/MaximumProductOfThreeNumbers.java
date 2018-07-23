c4158ac1ba7bce7b24d5452e4db4e269ac1ee878
torychow <522617505@qq.com>
2018-07-23 23:21:21 +0800

    Add test math abs

--- src/main/java/com/job/leetcode/array/maximumproductofthreenumbers/MaximumProductOfThreeNumbers.java
+++ src/main/java/com/job/leetcode/array/maximumproductofthreenumbers/MaximumProductOfThreeNumbers.java
@@ -4,8 +4,13 @@ package com.job.leetcode.array.maximumproductofthreenumbers;
  * https://leetcode-cn.com/problems/maximum-product-of-three-numbers/description/
  */
 public class MaximumProductOfThreeNumbers {
-    public int maximumProduct(int[] nums) {
 
+    public int maximumProduct(int[] nums) {
+        Math.abs(-5);
         return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-5));
     }
 }
