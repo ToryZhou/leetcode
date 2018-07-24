0023c71dfcb7eff54997d0c3bae94b6189c8ea58
torychow <522617505@qq.com>
2018-07-24 23:55:13 +0800

    Add max consecutive ones

---
+++ src/main/java/com/job/leetcode/array/maxconsecutiveones/MaxConsecutiveOnes.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.maxconsecutiveones;
+
+/**
+ * https://leetcode-cn.com/problems/max-consecutive-ones/description/
+ */
+public class MaxConsecutiveOnes {
+
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        return 0;
+    }
+}
