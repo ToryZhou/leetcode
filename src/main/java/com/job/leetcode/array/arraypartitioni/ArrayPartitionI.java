ac31362b58d247bd81ca2011696d234480f83e2b
torychow <522617505@qq.com>
2018-07-27 23:23:03 +0800

    Add array partition

--- src/main/java/com/job/leetcode/array/arraypartitioni/ArrayPartitionI.java
+++ src/main/java/com/job/leetcode/array/arraypartitioni/ArrayPartitionI.java
@@ -8,8 +8,22 @@ package com.job.leetcode.array.arraypartitioni;
 public class ArrayPartitionI {
 
     public int arrayPairSum(int[] nums) {
-        //todo
-        return 0;
-    }
 
+        // todo take too long time
+        int result = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    nums[j] = nums[i] + nums[j];
+                    nums[i] = nums[j] - nums[i];
+                    nums[j] = nums[j] - nums[i];
+                }
+            }
+
+        }
+        for (int i = 0; i < nums.length; i = i + 2) {
+            result += nums[i];
+        }
+        return result;
+    }
 }
