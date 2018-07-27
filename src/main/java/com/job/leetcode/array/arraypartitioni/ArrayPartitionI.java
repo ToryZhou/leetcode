38fbe976db9563e458aca29f4a98b70cc33be11c
toryzhou <522617505@qq.com>
2018-07-27 09:26:04 +0800

    Add array partition i

---
+++ src/main/java/com/job/leetcode/array/arraypartitioni/ArrayPartitionI.java
@@ -0,0 +1,15 @@
+package com.job.leetcode.array.arraypartitioni;
+
+/**
+ * Auther: toryzhou
+ * Date: 7/27/18 09:22
+ * https://leetcode-cn.com/problems/array-partition-i/description/
+ */
+public class ArrayPartitionI {
+
+    public int arrayPairSum(int[] nums) {
+        //todo
+        return 0;
+    }
+
+}
