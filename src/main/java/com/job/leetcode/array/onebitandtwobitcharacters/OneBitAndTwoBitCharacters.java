585dad045eef9eae79becb2bfb493083378e25c7
torychow <522617505@qq.com>
2018-07-28 21:46:12 +0800

    Add one bit and two bit characters

---
+++ src/main/java/com/job/leetcode/array/onebitandtwobitcharacters/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.onebitandtwobitcharacters;
+
+/**
+ * https://leetcode-cn.com/problems/1-bit-and-2-bit-characters/description/
+ */
+public class OneBitAndTwoBitCharacters {
+
+    public boolean isOneBitCharacter(int[] bits) {
+        // todo
+        return false;
+    }
+}
