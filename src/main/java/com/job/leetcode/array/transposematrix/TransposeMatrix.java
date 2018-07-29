035b1ce4a8fede99fb13c789b38fca9c1674764c
torychow <522617505@qq.com>
2018-07-29 23:35:56 +0800

    Complete transpose matrix

--- src/main/java/com/job/leetcode/array/transposematrix/TransposeMatrix.java
+++ src/main/java/com/job/leetcode/array/transposematrix/TransposeMatrix.java
@@ -4,8 +4,16 @@ package com.job.leetcode.array.transposematrix;
  * https://leetcode-cn.com/problems/transpose-matrix/description/
  */
 public class TransposeMatrix {
+
     public int[][] transpose(int[][] A) {
-        // todo
-        return null;
+        int x = A.length;
+        int y = A[0].length;
+        int[][] result = new int[y][x];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                result[j][i] = A[i][j];
+            }
+        }
+        return result;
     }
 }
