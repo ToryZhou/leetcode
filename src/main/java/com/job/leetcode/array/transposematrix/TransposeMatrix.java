c62a6b5197d735c417b883f2bb89e35c090b5522
torychow <522617505@qq.com>
2018-07-29 18:27:15 +0800

    Add transpose matrix

---
+++ src/main/java/com/job/leetcode/array/transposematrix/TransposeMatrix.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.transposematrix;
+
+/**
+ * https://leetcode-cn.com/problems/transpose-matrix/description/
+ */
+public class TransposeMatrix {
+    public int[][] transpose(int[][] A) {
+        // todo
+        return null;
+    }
+}
