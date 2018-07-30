91a491b61d0664bf3d5119241bc6f85fe78b28b1
torychow <522617505@qq.com>
2018-07-31 00:06:54 +0800

    Add majority element

---
+++ src/main/java/com/job/leetcode/array/majorityelement/MajorityElement.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.majorityelement;
+
+/**
+ * https://leetcode-cn.com/problems/majority-element/description/
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
