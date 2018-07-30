7ed61cd258ecd73146923071e644c95c1b4951c9
torychow <522617505@qq.com>
2018-07-30 22:52:45 +0800

    Add move zeroes

---
+++ src/main/java/com/job/leetcode/array/movezeroes/MoveZeroes.java
@@ -0,0 +1,15 @@
+package com.job.leetcode.array.movezeroes;
+
+/**
+ * https://leetcode-cn.com/problems/move-zeroes/description/
+ */
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]==0){
+
+            }
+        }
+    }
+}
