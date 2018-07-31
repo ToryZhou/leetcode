6aad51f9d54f5a3f057a5f77b392136ba4141f28
torychow <522617505@qq.com>
2018-07-31 22:59:06 +0800

    Complete majority element

--- src/main/java/com/job/leetcode/array/majorityelement/MajorityElement.java
+++ src/main/java/com/job/leetcode/array/majorityelement/MajorityElement.java
@@ -1,11 +1,26 @@
 package com.job.leetcode.array.majorityelement;
 
+import java.util.HashMap;
+import java.util.Set;
+
 /**
  * https://leetcode-cn.com/problems/majority-element/description/
  */
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            Integer integer = map.get(num);
+            integer = null == integer ? 0 : integer;
+            map.put(num, integer + 1);
+        }
+        Set<Integer> integers = map.keySet();
+        for (Integer key : integers) {
+            if (map.get(key) > nums.length / 2) {
+                return key;
+            }
+        }
         return 0;
     }
 }
