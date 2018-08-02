8ffac10224b03638ae5f14972f3b97e297a288f6
torychow <522617505@qq.com>
2018-08-02 23:38:01 +0800

    Add toeplitz matrix

---
+++ src/main/java/com/job/leetcode/array/toeplitzmatrix/ToeplitzMatrix.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.toeplitzmatrix;
+
+/**
+ * https://leetcode-cn.com/problems/toeplitz-matrix/description/
+ */
+public class ToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        // todo
+        return false;
+    }
+}
