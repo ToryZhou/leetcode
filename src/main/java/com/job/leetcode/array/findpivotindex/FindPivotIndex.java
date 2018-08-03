3f30823e441cc88ef55d1c519a38a917b19a929c
torychow <522617505@qq.com>
2018-08-04 00:36:17 +0800

    Add find pivot index

---
+++ src/main/java/com/job/leetcode/array/findpivotindex/FindPivotIndex.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.findpivotindex;
+
+/**
+ * https://leetcode-cn.com/problems/find-pivot-index/description/
+ */
+public class FindPivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        return 0;
+    }
+}
