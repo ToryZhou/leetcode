a856cf54c0b6c0a815009f851b7c63bc6abbb4f6
toryzhou <522617505@qq.com>
2018-08-03 09:36:31 +0800

    Add reshape the matrix

---
+++ src/main/java/com/job/leetcode/array/reshapethematrix/ReshapeTheMatrix.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.array.reshapethematrix;
+
+/**
+ * Auther: toryzhou
+ * Date: 8/3/18 09:35
+ * https://leetcode-cn.com/problems/reshape-the-matrix/description/
+ */
+public class ReshapeTheMatrix {
+
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        // todo
+        return null;
+    }
+}
