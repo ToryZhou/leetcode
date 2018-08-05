bc2f4790272fe08c615eac48a75081e4ecbf5f3e
torychow <522617505@qq.com>
2018-08-05 16:21:28 +0800

    Add degree of an array

---
+++ src/main/java/com/job/leetcode/array/degreeofanarray/DegreeOfAnArray.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.array.degreeofanarray;
+
+/**
+ * https://leetcode-cn.com/problems/degree-of-an-array/description/
+ */
+public class DegreeOfAnArray {
+
+
+    public int findShortestSubArray(int[] nums) {
+        //todo
+        return 0;
+    }
+
+}
