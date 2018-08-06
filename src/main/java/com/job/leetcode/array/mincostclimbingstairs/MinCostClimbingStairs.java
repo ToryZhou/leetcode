ccfc5144aca69d66ce1e41c9121dfea210f0aa3c
torychow <522617505@qq.com>
2018-08-06 23:57:09 +0800

    Add min cost climbing stairs

---
+++ src/main/java/com/job/leetcode/array/mincostclimbingstairs/MinCostClimbingStairs.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.mincostclimbingstairs;
+
+/**
+ * https://leetcode-cn.com/problems/min-cost-climbing-stairs/description/
+ */
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        // todo
+        return 0;
+    }
+}
