01d0203883ec2896fab48cb3cabb5e8236421764
torychow <522617505@qq.com>
2018-08-08 01:34:51 +0800

    Add k diff pairs in an array

---
+++ src/main/java/com/job/leetcode/array/kdiffpairsinanarray/KDiffPairsInAnArray.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.kdiffpairsinanarray;
+
+/**
+ * https://leetcode-cn.com/problems/k-diff-pairs-in-an-array/description/
+ */
+public class KDiffPairsInAnArray {
+
+    public int findPairs(int[] nums, int k) {
+        // todo
+        return 0;
+    }
+}
