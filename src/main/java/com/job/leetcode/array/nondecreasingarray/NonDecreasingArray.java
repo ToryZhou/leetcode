53ce9dfc40588f8e66a15413dc0c642103829f15
torychow <522617505@qq.com>
2018-08-11 23:48:07 +0800

    Add non decreasing array

---
+++ src/main/java/com/job/leetcode/array/nondecreasingarray/NonDecreasingArray.java
@@ -0,0 +1,13 @@
+package com.job.leetcode.array.nondecreasingarray;
+
+/**
+ * https://leetcode-cn.com/problems/non-decreasing-array/description/
+ */
+public class NonDecreasingArray {
+
+    public boolean checkPossibility(int[] nums) {
+        // todo
+        return false;
+    }
+
+}
