4ad11eacff3ffe0d5ec36a638886cefb477a9cbc
torychow <522617505@qq.com>
2018-08-16 22:37:03 +0800

    Add largest number at least twice of others

---
+++ src/main/java/com/job/leetcode/array/largestnumberatleasttwiceofothers/LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.largestnumberatleasttwiceofothers;
+
+/**
+ * https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/description/
+ */
+public class LargestNumberAtLeastTwiceOfOthers {
+
+    public int dominantIndex(int[] nums) {
+        return 0;
+    }
+}
