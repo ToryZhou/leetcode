866d20be29e77c43d5c0298936ab2914d8e0b3e3
torychow <522617505@qq.com>
2018-08-19 23:00:12 +0800

    Add max area of island

---
+++ src/main/java/com/job/leetcode/array/maxareaofisland/MaxAreaOfIsland.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.array.maxareaofisland;
+
+/**
+ * https://leetcode-cn.com/problems/max-area-of-island/description/
+ */
+public class MaxAreaOfIsland {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        //todo
+        return 0;
+
+    }
+
+}
