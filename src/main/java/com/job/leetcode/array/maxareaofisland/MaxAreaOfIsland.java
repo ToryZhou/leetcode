d0b1b3753f4e670ab60380a0cc11167a31ad1083
torychow <522617505@qq.com>
2018-08-20 19:49:52 +0800

    Edit max area of island

--- src/main/java/com/job/leetcode/array/maxareaofisland/MaxAreaOfIsland.java
+++ src/main/java/com/job/leetcode/array/maxareaofisland/MaxAreaOfIsland.java
@@ -7,8 +7,33 @@ public class MaxAreaOfIsland {
 
     public int maxAreaOfIsland(int[][] grid) {
         //todo
-        return 0;
+        int max = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                int temp = 0;
+                if (grid[i][j] == 1) {
+                    for (int m = j; j < grid[0].length; j++) {
+                        if (grid[i][m] == 1) {
+                            temp += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    for (int n = i; n < grid.length; n++) {
+                        if (grid[n][j] == 1) {
+                            temp += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                if (temp > max) {
+                    max = temp;
+                }
 
+            }
+        }
+        return max;
     }
 
 }
