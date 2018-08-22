4e04bc71b538106595a71596541bcaa1fd8a9339
toryzhou <522617505@qq.com>
2018-08-22 08:59:33 +0800

    Add Long Continuous Increasing Sub sequence

---
+++ src/main/java/com/job/leetcode/array/longestcontinuousincreasingsubsequence/LongContinuousIncreasingSubsequence.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.array.longestcontinuousincreasingsubsequence;
+
+/**
+ * Auther: toryzhou
+ * Date: 8/22/18 08:58
+ * https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence/description/
+ */
+public class LongContinuousIncreasingSubsequence {
+    public int findLengthOfLCIS(int[] nums) {
+        return 0;
+    }
+}
