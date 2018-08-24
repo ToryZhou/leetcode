2c8cfac101c7196894727f9511c5056dab21ec65
toryzhou <522617505@qq.com>
2018-08-24 12:36:29 +0800

    Add Fair Candy Swap

---
+++ src/main/java/com/job/leetcode/array/faircandyswap/FairCandySwap.java
@@ -0,0 +1,13 @@
+package com.job.leetcode.array.faircandyswap;
+
+/**
+ * Auther: toryzhou
+ * Date: 8/24/18 12:35
+ * https://leetcode-cn.com/problems/fair-candy-swap/description/
+ */
+public class FairCandySwap {
+
+    public int[] fairCandySwap(int[] A, int[] B) {
+        return null;
+    }
+}
