48a705cf08db800a6312a5aafa2fa97487e73b9a
torychow <522617505@qq.com>
2018-09-02 00:06:07 +0800

    Add Game of Life

---
+++ src/main/java/com/job/leetcode/array/gameoflife/GameOfLife.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.array.gameoflife;
+
+/**
+ * https://leetcode-cn.com/problems/game-of-life/description/
+ */
+public class GameOfLife {
+
+    public void gameOfLife(int[][] board) {
+        //todo
+    }
+}
