1bf532cca602ad3c152ab22a48758c61c63d1de4
torychow <522617505@qq.com>
2018-09-08 23:20:59 +0800

    Add range sum query immutable

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/rangesumqueryimmutable/NumArray.java
@@ -0,0 +1,17 @@
+package com.job.leetcode.dynamicprogramming.rangesumqueryimmutable;
+
+/**
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ */
+public class NumArray {
+
+    public NumArray(int[] nums) {
+
+    }
+
+    public int sumRange(int i, int j) {
+        //todo
+
+        return 0;
+    }
+}
