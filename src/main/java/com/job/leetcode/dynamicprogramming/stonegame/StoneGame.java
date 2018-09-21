1ad8e4b4d0ecdedf611efd8436862a5be369a6d2
torychow <522617505@qq.com>
2018-09-22 02:58:26 +0800

    Add stone game

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/stonegame/StoneGame.java
@@ -0,0 +1,13 @@
+package com.job.leetcode.dynamicprogramming.stonegame;
+
+/**
+ * https://leetcode-cn.com/problems/stone-game/description/
+ */
+public class StoneGame {
+    public boolean stoneGame(int[] piles) {
+
+        //todo
+        return false;
+
+    }
+}
