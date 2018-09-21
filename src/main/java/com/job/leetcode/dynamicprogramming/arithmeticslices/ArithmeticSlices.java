3bea59bab6174b681dc5a74dbc0d5315bbaa9d97
torychow <522617505@qq.com>
2018-09-21 23:29:19 +0800

    Add arithmetic slices

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/arithmeticslices/ArithmeticSlices.java
@@ -0,0 +1,11 @@
+package com.job.leetcode.dynamicprogramming.arithmeticslices;
+
+/**
+ * https://leetcode-cn.com/problems/arithmetic-slices/description/
+ */
+public class ArithmeticSlices {
+    public int numberOfArithmeticSlices(int[] A) {
+        // todo
+        return 0;
+    }
+}
