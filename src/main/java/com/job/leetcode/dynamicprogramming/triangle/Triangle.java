e80546d9309d901f6603e91a5dc3fe13642ede3e
torychow <522617505@qq.com>
2018-09-23 21:24:20 +0800

    Add triangle

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/triangle/Triangle.java
@@ -0,0 +1,14 @@
+package com.job.leetcode.dynamicprogramming.triangle;
+
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/triangle/description/
+ */
+public class Triangle {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // todo
+        return 0;
+    }
+}
