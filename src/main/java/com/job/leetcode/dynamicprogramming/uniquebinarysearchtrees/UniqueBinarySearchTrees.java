4da8b0b97ee35276451ee87afd9a0d50e1a24405
torychow <522617505@qq.com>
2018-09-27 22:19:16 +0800

    Add unique binary search trees

---
+++ src/main/java/com/job/leetcode/dynamicprogramming/uniquebinarysearchtrees/UniqueBinarySearchTrees.java
@@ -0,0 +1,12 @@
+package com.job.leetcode.dynamicprogramming.uniquebinarysearchtrees;
+
+/**
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/description/
+ */
+public class UniqueBinarySearchTrees {
+
+    public int numTrees(int n) {
+        //todo
+        return 0;
+    }
+}
